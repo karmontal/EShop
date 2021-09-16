73a8f3bb811afb934effb328113badfd574577ef
KTALLA <karmontal86@gmail.com>
2021-09-16 16:55:33 +0300

    Complete Token

--- EShop/APIAUTHORIZATIONSERVERPROVIDER.cs
+++ EShop/APIAUTHORIZATIONSERVERPROVIDER.cs
@@ -18,26 +18,25 @@ namespace EShop
         {
 
             var identity = new ClaimsIdentity(context.Options.AuthenticationType);
-
-            //if (context.UserName == "admin" && context.Password == "admin")
-            //{
-            //    identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
-            //    identity.AddClaim(new Claim("username", "admin"));
-            //    identity.AddClaim(new Claim(ClaimTypes.Name, "Hi Admin"));
-            //    context.Validated(identity);
-            //}
-            //else if (context.UserName == "user" && context.Password == "user")
-            //{
-            //    identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
-            //    identity.AddClaim(new Claim("username", "user"));
-            //    identity.AddClaim(new Claim(ClaimTypes.Name, "Hi User"));
-            //    context.Validated(identity);
-            //}
-            //else
-            //{
-            //    context.SetError("invalid_grant", "Provided username and password is incorrect");
-            //    return;
-            //}
+            if (context.UserName == "admin" && context.Password == "admin")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+                identity.AddClaim(new Claim("username", "admin"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Hi Admin"));
+                context.Validated(identity);
+            }
+            else if (context.UserName == "user" && context.Password == "user")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+                identity.AddClaim(new Claim("username", "user"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Hi User"));
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("invalid_grant", "Provided username and password is incorrect");
+                return;
+            }
         }
     }
 }
